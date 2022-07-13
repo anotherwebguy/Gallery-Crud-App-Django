61e9580d4f9378441a7cf2a7a2f0a59266f31323
Mohit <mohitrajputms123@gmail.com>
2022-07-14 00:45:31 +0530

    bug fixes

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -124,8 +124,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
